ca509f28b705429f230091758ef25caeeed8cf6c
ylim1699 <ylim.0348@gmail.com>
2024-11-09 21:57:44 -0600

    variables input output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        string FirstName = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name?");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"your name is {LastName}, {FirstName} {LastName}");
+
     }
 }
